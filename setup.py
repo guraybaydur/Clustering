64c531f40a54a3d2871b4ca472cf12abaffef99e
guraybaydur <guraybaydur@gmail.com>
2021-02-13 13:05:52 +0300

    modified to run in new env

--- setup.py
+++ setup.py
@@ -13,15 +13,16 @@ runtime_packages = [
 "kiwisolver==1.1.0",
 "MarkupSafe==1.1.1",
 "matplotlib==3.1.1",
-"numpy==1.17.0",
-"pandas==0.25.0",
+"numpy==1.20.1",
+"pandas==1.2.2",
 "profilehooks==1.11.0",
 "pyparsing==2.4.1.1",
 "python-dateutil==2.8.0",
 "pytz==2019.1",
-"scipy==1.3.0",
+"scipy==1.6.0",
 "six==1.12.0",
 "Werkzeug==0.15.5",
+"pyflann-py3==0.1.0"
 ]
 
 setup(
